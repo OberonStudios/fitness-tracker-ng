9548eb6bd3eb8f554cb0d47fdc181893f7d9cf49
aakatev <aakatev@mail.ccsf.edu>
2019-02-12 23:10:51 -0800

    Material hooked-up; Apps key components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TrainingComponent } from './training/training.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'train', component: TrainingComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
